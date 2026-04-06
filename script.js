167be6078fc222aa616fa8a8f2c1bd8ba98ae992
andikanauf <160825174+andikanauf@users.noreply.github.com>
2026-04-06 13:59:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const typingText = document.querySelector('.typing');
+
+const words = [
+  'Frontend Developer',
+  'UI Designer',
+  'Laravel Enthusiast',
+  'Creative Student'
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const currentWord = words[wordIndex];
+
+  if (!isDeleting) {
+    typingText.textContent = currentWord.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === currentWord.length) {
+      isDeleting = true;
+      setTimeout(typeEffect, 1200);
+      return;
+    }
+  } else {
+    typingText.textContent = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+      wordIndex = (wordIndex + 1) % words.length;
+    }
+  }
+
+  setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
